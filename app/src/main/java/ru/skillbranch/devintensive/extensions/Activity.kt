b41336b042e93285f3e2578eb7dd2a4f8092b217
Константин <ferrum9696@mail.ru>
2019-08-23 19:37:19 +0300

    home_3

--- app/src/main/java/ru/skillbranch/devintensive/extensions/Activity.kt
+++ app/src/main/java/ru/skillbranch/devintensive/extensions/Activity.kt
@@ -1,5 +1,11 @@
 package ru.skillbranch.devintensive.extensions
 
-fun hideKeyboard(){
+import android.app.Activity
+import android.content.Context
+import android.view.inputmethod.InputMethodManager
 
-}
+//fun Activity.hideKeyboard(){
+//    if (currentFocus != null) {
+//        InputMethodManager imm = (InputMethodManager) activity.getSystemService(Context.INPUT_METHOD_SERVICE)
+//        imm.hideSoftInputFromWindow(currentFocus.getWindowToken(), 0);
+//}
--- app/src/main/java/ru/skillbranch/devintensive/models/Bender.kt
+++ app/src/main/java/ru/skillbranch/devintensive/models/Bender.kt
@@ -17,7 +17,8 @@ class Bender(var status: Status = Status.NORMAL, var question: Question = Questi
             "Отлично - ты справился\n${question.question}" to status.color
         } else {
             if (status == Status.CRITICAL) {
-                status = status.nextStatus()
+                status = Status.NORMAL
+                question = Question.NAME
                 "Это неправильный ответ. Давай все по новой\n${question.question}" to status.color
             } else {
                 status = status.nextStatus()
